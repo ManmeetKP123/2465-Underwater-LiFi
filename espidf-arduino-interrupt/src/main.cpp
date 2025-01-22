b39473f067e254565442cc10d9a3bc871c7f0f66
Tara Kong <tarakong01@gmail.com>
2025-01-22 15:25:53 -0800

    Working implementation of  modulation for a hardcoded string, with start-of-frame and message length bytes

--- espidf-arduino-interrupt/src/main.cpp
+++ espidf-arduino-interrupt/src/main.cpp
@@ -5,6 +5,15 @@
 #include "freertos/xtensa_timer.h"
 #include "esp_intr_alloc.h"
 
+#define MODULATED_BYTES_MAX_LEN 1028
+#define SIGNAL_TO_DATA_RATIO    1
+#define BITS_PER_BYTE           8
+#define PULSE_DELAY_MS          100 /* Represents ~1/2 period for a single pulse. */
+
+#define LED_GPIO                8
+
+uint8_t modulatedBytes[MODULATED_BYTES_MAX_LEN];
+
 hw_timer_t *My_timer = NULL;
 TaskHandle_t Task1;
 
@@ -22,86 +31,147 @@ void Task1code( void * parameter) {
 }
 
 /* Functions for encoding and sending data. */
-void modulateByte(char byte, uint8_t *modulatedByte, size_t &modulatedByteLen) {
+void modulateByte(uint8_t byte, uint8_t *modulatedByte, size_t &modulatedByteLen) {
   /* The number of periods that represent a single bit of data. */
-  size_t signalToDataRatio = 3U;
 
   size_t index = 0U;
   uint8_t nextBit = 0U;
 
   /* TODO: make sure the modulatedByte buffer is big enough. */
-  modulatedByteLen = signalToDataRatio * 8U;
+  modulatedByteLen = SIGNAL_TO_DATA_RATIO * BITS_PER_BYTE;
 
-  printf("Original character: ");
+  Serial.print("\n\nOriginal byte: ");
   for(int bitIndex = 7; bitIndex >= 0; bitIndex--) {
-    nextBit = byte & 0x80; // get left-most bit.
-    printf("%d", nextBit); // debugging print
+    nextBit = (byte & 0x80) >> 7; // get left-most bit.
+    Serial.print(nextBit); // debugging print
 
     /* Add multiple periods of the same pulse, to modulate the signal. */
-    for(size_t pulseCount = 0; pulseCount < signalToDataRatio; pulseCount++) {
+    for(size_t pulseCount = 0; pulseCount < SIGNAL_TO_DATA_RATIO; pulseCount++) {
       modulatedByte[index] = nextBit;
+      index++;
     }
 
     byte <<= 1; // left bit-shift once.
   }
 
   /* debugging print: print the final modulated character. */
-  printf("\n\nModulated character: ");
-  for(size_t index = 0U; index < signalToDataRatio * 8U; index++) {
-    printf("%d", nextBit);
+  Serial.print("\nModulated byte: ");
+  for(size_t index = 0U; index < SIGNAL_TO_DATA_RATIO * BITS_PER_BYTE; index++) {
+    Serial.print(modulatedByte[index]);
   }
 }
 
-void modulateString(const char *bytes, size_t modulatedBytesLen) {
-  uint8_t modulatedString[1048U];
+int modulateString(const char *bytes, size_t bytesLen, uint8_t *modulatedBytes, size_t modulatedBytesLen) {
   size_t pos = 0U;
+  size_t modulatedLen = 0U;
 
-  for(size_t index = 0U; index < modulatedBytesLen; index++) {
+  /* First add the length of the message to the buffer. */
+
+  /* Modulate the byte representing the length of the message. */
+  uint8_t modulatedMsgLen[BITS_PER_BYTE * SIGNAL_TO_DATA_RATIO + 1U];
+  modulateByte(bytesLen, &modulatedMsgLen[0U], modulatedLen);
+
+  /* Copy the modulated byte into the modulated string. */
+  memcpy(&modulatedBytes[pos], &modulatedMsgLen[0U], modulatedLen);
+  pos += modulatedLen;
+
+  /* Modulate the message. */
+  for(size_t index = 0U; index < bytesLen; index++) {
     uint8_t modulatedByte[50U];
-    size_t modulatedLen = 0U;
 
     /* Modulate the next byte. */
     modulateByte(bytes[index], &modulatedByte[0U], modulatedLen);
 
     /* Copy the modulated byte into the modulated string. */
-    memcpy(&modulatedString[pos], &modulatedByte[0U], modulatedLen);
+    memcpy(&modulatedBytes[pos], &modulatedByte[0U], modulatedLen);
 
     /* Update pos to point to the end of the buffer. */
     pos += modulatedLen;
+
+    /* Make sure that the pos doesn't overflow the modulatedBytesLen. */
+    if(pos >= modulatedBytesLen) {
+      Serial.println("ERROR - modulated bytes' size greater than the max size of the buffer!");
+      return -1;
+    }
   }
 
   /* debugging print: print the final modulated string. */
-  printf("\n\nModulated string: ");
+  Serial.print("\n\nModulated string: ");
   for(size_t index = 0U; index < pos; index++) {
-    printf("%d", modulatedString[index]);
+    Serial.print(modulatedBytes[index]);
+  }
+
+  return 0;
+}
+
+void pulseBinary1() {
+  digitalWrite(LED_GPIO, HIGH);
+  delay(PULSE_DELAY_MS);
+  digitalWrite(LED_GPIO, LOW);
+  delay(PULSE_DELAY_MS);
+}
+
+void pulseBinary0() {
+  digitalWrite(LED_GPIO, LOW);
+  delay(PULSE_DELAY_MS);
+  digitalWrite(LED_GPIO, LOW);
+  delay(PULSE_DELAY_MS);
+}
+
+
+/*!
+ * @brief   Outputs the Start of Frame bit sequence
+ * @details Outputs the following sequence: 11111111, modulated, to indicate the start of the message.
+ */
+void outputStartOfFrame() {
+  /* Output a high. */
+  for(int i = 0; i < BITS_PER_BYTE * SIGNAL_TO_DATA_RATIO; i++) {
+      pulseBinary1();
   }
 }
 
 void setup(){
-  pinMode(8,OUTPUT);
-  pinMode(32,OUTPUT);
+  pinMode(LED_GPIO, OUTPUT);
+  Serial.begin(115200);
   
-  xTaskCreatePinnedToCore( // create a task on core 0 to make sure that still works with core 1 interrupts disabled later
-      Task1code, /* Function to implement the task */
-      "Task1", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &Task1,  /* Task handle. */
-      0); /* Core where the task should run */
+  // xTaskCreatePinnedToCore( // create a task on core 0 to make sure that still works with core 1 interrupts disabled later
+  //     Task1code, /* Function to implement the task */
+  //     "Task1", /* Name of the task */
+  //     10000,  /* Stack size in words */
+  //     NULL,  /* Task input parameter */
+  //     0,  /* Priority of the task */
+  //     &Task1,  /* Task handle. */
+  //     0); /* Core where the task should run */
 
   // My_timer = timerBegin(0, 80, true);
   // timerAttachInterrupt(My_timer, &onTimer, true);
   // timerAlarmWrite(My_timer, 1000, true);
   // timerAlarmEnable(My_timer);
-  ESP_INTR_DISABLE(XT_TIMER_INTNUM); // disables the tick interrupt
+  // ESP_INTR_DISABLE(XT_TIMER_INTNUM); // disables the tick interrupt
 
   /* Debugging - hardcode the string to send. */
   const char message[] = "hello";
-  modulateString(&message[0U], strlen(message));
+  int ret = modulateString(&message[0U], strlen(message), &modulatedBytes[0U], MODULATED_BYTES_MAX_LEN);
+
+  /* Delay one second before starting transmission. */
+  delay(1000);
+  
+  /* Output the start-of-frame sequence. */
+  outputStartOfFrame();
+
+  /* Output the signal. */
+  for(int i = 0; i < strlen(message) * BITS_PER_BYTE * SIGNAL_TO_DATA_RATIO; i++) {
+    if(modulatedBytes[i] == 1) {
+      /* Output a high. */
+      pulseBinary1();
+    } else {
+      /* Output a low. */
+      pulseBinary0();
+    }
+  }
+  Serial.print("\nDone modulation.");
 }
 
 void loop(){
-  digitalWrite(8,HIGH); // toggle a pin quickly - if everything is working there will be no gaps in the toggling signal
-  digitalWrite(8,LOW);
+  /* TODO: implement. */
 }
