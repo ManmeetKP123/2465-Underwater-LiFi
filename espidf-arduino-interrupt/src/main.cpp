1fc6003492f002009fd819a57afea8296f8dbcbc
Manmeet <meetkaur@student.ubc.ca>
2025-02-05 12:27:47 -0800

    demodulation code using timer, from sunday

--- espidf-arduino-interrupt/src/main.cpp
+++ espidf-arduino-interrupt/src/main.cpp
@@ -6,9 +6,11 @@
 #define PHOTO_PIN 14
 #define MODULATION_FREQ 1 // for VLC
 #define BAUD_RATE 115200 // for serial communication
+#define SAMPLING_TIME_MS 100
+#define SAMPLES_PER_PERIOD 10
 #define THRESHOLD 500 // Adjust based on photodiode sensitivity
 #define START_BIT_DURATION 8U
-#define BUFFER_SIZE 256
+#define BUFFER_SIZE 8
 
 /**
  * VOLATILE DECLARATIONS
@@ -17,23 +19,30 @@ volatile bool sofDetected = false;
 volatile uint8_t risingPulseCount = 0;
 volatile uint8_t fallingPulseCount = 0;
 volatile uint8_t lastState = 0;
+volatile bool byteReady =  false;
+volatile uint8_t sampleBuffer[SAMPLES_PER_PERIOD];
+volatile uint8_t bitCounter = 0;
+volatile bool periodComplete = false;
+volatile unsigned long lastSampleTime = 0; 
 
 /**
- * GLOBAL VARIABLE
+ * GLOBAL VARIABLES
 */
-hw_timer_t *My_timer = NULL;
-TaskHandle_t Task1;
-byte bitBuffer[BUFFER_SIZE]; // for decoding the incoming bits
+uint8_t bitBuffer[BUFFER_SIZE]; // for decoding the incoming bits
 int bufferIndex = 0;
+bool lengthDetected = false;
+hw_timer_t *timer = NULL;
+bool detectedOnce = false;
 
-
-void IRAM_ATTR onTimer(){
-  digitalWrite(32, HIGH);
-  digitalWrite(32, LOW);
-}
-
-void IRAM_ATTR start_measurement() {
+/**
+ * HELPER FUNCTIONS
+*/
+void IRAM_ATTR startOfFrameISR() {
+  // read the GPIO pin directly via port manipulation, digitalRead might cause jitters
+  // int currentState = (GPIO_IN_REG & (1 << PHOTO_PIN)) ? 1 : 0;
+ 
   int currentState = digitalRead(PHOTO_PIN);
+
   if (currentState){ // currentState HI so rising edge detection
     risingPulseCount++;
   } else { // currentState LO so falling edge detection
@@ -42,44 +51,79 @@ void IRAM_ATTR start_measurement() {
 
   if (risingPulseCount >=8 && fallingPulseCount >= 8) { // we've detected 16 successful transitions
     sofDetected = true;
-    detachInterrupt(PHOTO_PIN); // detach until we need to start detecting SOF again
   }
 }
 
-void Task1code( void * parameter) {
-  for(;;) {
-    digitalWrite(32, HIGH);
-    digitalWrite(32, LOW);
-    vTaskDelay(1);
+void IRAM_ATTR samplingISR() {
+  sampleBuffer[bitCounter] = digitalRead(PHOTO_PIN);
+  bitCounter++;
+
+  if (bitCounter >= SAMPLES_PER_PERIOD) {
+    periodComplete = true;
+    bitCounter = 0;
   }
 }
 
+/**
+ * MAIN
+*/
 void setup(){
   pinMode(32,OUTPUT);
-  
-  xTaskCreatePinnedToCore( // create a task on core 0 to make sure that still works with core 1 interrupts disabled later
-      Task1code, /* Function to implement the task */
-      "Task1", /* Name of the task */
-      10000,  /* Stack size in words */
-      NULL,  /* Task input parameter */
-      0,  /* Priority of the task */
-      &Task1,  /* Task handle. */
-      0); /* Core where the task should run */
-
-  // My_timer = timerBegin(0, 80, true);
-  // timerAttachInterrupt(My_timer, &onTimer, true);
-  // timerAlarmWrite(My_timer, 1000, true);
-  // timerAlarmEnable(My_timer);
   ESP_INTR_DISABLE(XT_TIMER_INTNUM); // disables the tick interrupt
 
   Serial.begin(BAUD_RATE);
   pinMode(PHOTO_PIN, INPUT);
-  attachInterrupt(PHOTO_PIN, start_measurement, CHANGE);
+  attachInterrupt(PHOTO_PIN, startOfFrameISR, CHANGE);
+
+  timer = timerBegin(0, 80, true);
+  // slowest the timer interrupt can be implemented is 1220.7Hz;
+  timerAttachInterrupt(timer, &samplingISR, true);
+  // count up from 0 to 99999 for 1s timer; sampling rate of 20kHz
+  timerAlarmWrite(timer, SAMPLING_TIME_MS * 1000, true);
+  Serial.println("AHHHHHHHHHH");
 }
 
 void loop() {
   if (sofDetected) {
+    detachInterrupt(PHOTO_PIN); // detach until we need to start detecting SOF again
     Serial.println("Start of frame detected");
     sofDetected = false;
+    
+    timerAlarmEnable(timer);   // Start sampling
+
+    bufferIndex = 0;
+  } else if (periodComplete) {
+    periodComplete = false;
+
+    int onesCount = 0, zerosCount = 0;
+
+    for (int i = 0; i < SAMPLES_PER_PERIOD; i++) {
+      if (sampleBuffer[i]){
+        onesCount++;
+      } else {
+        zerosCount++;
+      }
+    }
+
+    uint8_t pulseBit = (onesCount > (zerosCount - 5)) ? 1 : 0;
+    bitBuffer[bufferIndex] = pulseBit;
+    bufferIndex++;
+
+
+    if (bufferIndex >= 8U) {
+      Serial.print("Collected Byte: ");
+      for (int i = 0; i < 8U; i++){
+        Serial.print(bitBuffer[i]);
+      }
+      Serial.println();
+      bufferIndex = 0;
+    }
   }
+
+  // if (byteReady) {
+  //   byteReady = false;
+
+  //   Serial.print("Expected length of signal: ");
+  //   Serial.println(currentByte, DEC);
+  // }
 }
